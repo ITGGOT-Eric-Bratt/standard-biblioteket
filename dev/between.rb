671237a3cf4625933360305039b03659004b0e85
ITGGOT-Eric-Bratt <eric.bratt@itggot.se>
2018-04-18 13:41:51 +0200

    Added Between and Betweenstrict

--- dev/between.rb
+++ dev/between.rb
@@ -1,6 +1,6 @@
 def between(x, y, z)
     output = false
-    if x > y && x < z
+    if x >= y && x <= z
         output = true
     end
     return output
---
+++ lib/betweenstrict.rb
@@ -0,0 +1,7 @@
+def betweenstrict(x, y, z)
+    output = false
+    if x > y && x < z
+        output = true
+    end
+    return output
+end
