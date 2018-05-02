19a38f62fa5408cb89dd46919a8bffe3dd730a13
ITGGOT-Eric-Bratt <eric.bratt@itggot.se>
2018-05-02 14:46:06 +0200

    added concat

---
+++ lib/concat.rb
@@ -0,0 +1,13 @@
+# Public: Concatenates two arrays
+#
+# arr1, arr2 - The arrays
+#
+# Examples
+#
+#   concat([1, 2, 3], [1, 2, 3])
+#   # => [1, 2, 3, 1, 2, 3]
+#
+# Returns the concatenated arrays
+def concat(arr1, arr2)
+    return arr1 + arr2
+end
