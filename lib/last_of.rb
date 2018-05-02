58d56115d0f7ebd07694f7b2dfe6f811a7981b33
ITGGOT-Eric-Bratt <eric.bratt@itggot.se>
2018-05-02 14:46:33 +0200

    added last_of

---
+++ lib/last_of.rb
@@ -0,0 +1,13 @@
+# Public: Returns the last object in an array
+#
+# arr - The array
+#
+# Examples
+#
+#   last_of([1, 2, 3])
+#   # => 3
+#
+# Returns the last object in the array
+def last_of(arr)
+    return arr[-1]
+end
