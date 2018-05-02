41c8532f5284b3eec3748b28df4248e4e34a1d26
ITGGOT-Eric-Bratt <eric.bratt@itggot.se>
2018-05-02 14:46:40 +0200

    added power

---
+++ lib/power.rb
@@ -0,0 +1,14 @@
+# Public: Displays a number to the power of another number
+#
+# siffra1 - The integer that is the base
+# siffra2 - The integer that is the exponent
+#
+# Examples
+#
+#   power(3, 2)
+#   # => 9
+#
+# Returns the power of the numbers
+def power(siffra1, siffra2)
+    return  siffra1**siffra2
+end
