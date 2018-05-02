c4a977f774c2cafcdc49cea51b06d49f8b5ad789
ITGGOT-Eric-Bratt <eric.bratt@itggot.se>
2018-05-02 14:46:25 +0200

    added first_of

---
+++ lib/first_of.rb
@@ -0,0 +1,14 @@
+# Public: Returns the first object in an array
+#
+# arr - The array
+#
+# Examples
+#
+#   first_of([1, 2, 3])
+#   # => 1
+#
+# Returns the first object in the array
+def first_of(arr)
+    return arr[0]
+end
+
